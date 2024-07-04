cb4d671218bb7fb5782333df9eb06c1c69c3c3e4
Test46Jenkins <158430604+Test46Jenkins@users.noreply.github.com>
2024-07-04 11:29:14 +0530

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+Sudo chown -R ec2-user:ec2-user /home/ec2-user/apps
 cd /home/ec2-user/apps
 curl -fsSL https://deb.nodesource.com/setup_20.x | sudo -E bash -
 sudo yum install -y nodejs
